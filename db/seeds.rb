e18f1b8064e4807cb6082f3e91c18aade1849c8a
mborreros <mlbsindia@gmail.com>
2022-03-24 15:50:08 -0400

    corrected model names, added user seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,5 +1,10 @@
-puts "🌱 Seeding spices..."
-
-# Seed your database here
-
-puts "✅ Done seeding!"
+puts "🌱👤 Seeding user data... 👤🌱"
+# user seed data
+6.times do |index|
+  User.create(
+    name: Faker::Name.first_name,
+    bio: Faker::Lorem.sentence(word_count: 7),
+    join_date: Faker::Date.between(from: 30.days.ago, to: Date.today)
+  )
+end
+puts "✅ User data seeded ✅"

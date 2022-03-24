50786113d22677509f05fd210d45d3e2a64a58b1
mborreros <mlbsindia@gmail.com>
2022-03-24 14:50:35 -0400

    created puzzles table

---
+++ db/migrate/20220324184544_create_puzzles.rb
@@ -0,0 +1,15 @@
+class CreatePuzzles < ActiveRecord::Migration[6.1]
+  def change
+    create_table :puzzles do |t|
+      t.string :title
+      t.integer :pieces
+      t.string :manufacturer
+      t.string :style
+      t.string :purchase_link 
+      t.integer :price
+      t.string :image
+      t.boolean :owned
+      t.timestamp :added_to_collection
+    end
+  end
+end

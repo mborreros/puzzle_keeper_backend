878f803628c39b33cccf7fe881aa3fd63f82b239
mborreros <mlbsindia@gmail.com>
2022-03-24 16:06:46 -0400

    added puzzle seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,5 +1,4 @@
-puts "🌱👤 Seeding user data... 👤🌱"
-# user seed data
+puts "👤 Seeding user data... 🌱"
 6.times do |index|
   User.create(
     name: Faker::Name.first_name,
@@ -7,4 +6,19 @@ puts "🌱👤 Seeding user data... 👤🌱"
     join_date: Faker::Date.between(from: 30.days.ago, to: Date.today)
   )
 end
-puts "✅ User data seeded ✅"
+puts "✨ User data seeded ✨"
+
+puts "🧩 Seeding puzzle data... 🌱"
+25.times do |index|
+  manufacturers = ["Ravensburger", "MB", "Eurographics", "Buffalo Games", "Bits and Pieces", "Galison"]
+  Puzzle.create(
+    # need to think of better way to get more believable titles
+    title: Faker::Verb.simple_present.capitalize,
+    pieces: 1000,
+    manufacturer: manufacturers.sample,
+    price: Faker::Number.decimal(l_digits: 2),
+    owned: true,
+    added_to_collection: Faker::Date.between(from: 60.days.ago, to: Date.today)
+  )
+end
+puts "✨ Puzzle data seeded ✨"
